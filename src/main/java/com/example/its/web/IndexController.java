a35db023d97ecc789c8ba6fdf9e83ef29ef69912
Hiro1120 <tnero1120@gmail.com>
2022-06-12 19:56:26 +0900

    課題作成ページの実装

--- src/main/java/com/example/its/web/IndexController.java
+++ src/main/java/com/example/its/web/IndexController.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class IndexController {
     @GetMapping//("/")省略可
-    //--ハンドラメソッド--
     public String index(){
         return "index";//SpringMVCの機能により拡張子は不要
     }
-    //--ハンドラメソッド--/
 }
