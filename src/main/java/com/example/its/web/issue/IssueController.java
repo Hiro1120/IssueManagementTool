84f97c4a7214f802bf962610cdf267a776ba23bf
Hiro1120 <tnero1120@gmail.com>
2022-06-13 23:11:09 +0900

    viewからデータを受け取る機能の実装

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -6,27 +6,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 @Controller
 @RequestMapping("/issues")
-//final付与されているfieldかつ初期化されていないコンストラクタを自動生成する
 @RequiredArgsConstructor
 public class IssueController {
     private final IssueService issueService ;
     //Get/issues
     @GetMapping
-    public String showList(Model model){ //メソッドの呼び出し時にSpringからModelのインスタンスを呼び出すことができる
-        //Thymeleafにオブジェクトを渡すことができるメソッド
-        //第1引数：Thymeleafで参照する際のkey,第2引数はThymeleafに渡したいオブジェクト
+    public String showList(Model model){
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
     }
     @GetMapping("/creationForm")
-    public String showCreationForm(){
+    public String showCreationForm(@ModelAttribute IssueForm form){
         return "/issues/creationForm";//resources/templatesからの相対パスを記載(拡張子は書かない)
     }
-
-
+    //POST/issues
+    @PostMapping
+    public String create(IssueForm form, Model model){
+        //TODO データの永続化
+        return showList(model);//TODO リロードボタン対策が必要
+    }
 }
